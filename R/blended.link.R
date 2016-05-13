89a9a25ddc00256cab2bb737da0b904d58ddc9c5
ROBERT GRAHAM CLARK <robertgrahamclark@gmail.com>
2016-05-13 11:37:24 +1000

    Package has been greatly simplified, and now contains just one function (blendedLink). The blendedLink now makes use of lexical scoping.

--- R/blended.link.R
+++ R/blended.link.R
@@ -1,24 +1,25 @@
-#' Link function which equals log function between user-specified cutoffs, and a scaled
-#'   user-specified link function for linear predictors below the first cutoff and above
-#'   the second cutoff.
+#' Link object where the link function equals a specified link function for values
+#' of the mean up to a specified cutoff, and a smooth linear-rescaling of a
+#' different link function when the mean is above the cutoff
 #'
-#' This function produces a blended object for a specified cutover and link function
+#' This function produces a blended object for a specified cutover and link functions
 #' for use in binary regression using glm or glm2.
 #'
 #' @details
-#' This function returns a link function which which equals log function for
-#' values of the fitted probability between cutover[1] and cutover[2].
-#' For values less than cutover[1] or greater than cutover[2], a linearly scaled user-specified link
-#' is used (typically logit is recommended). The linear scaling is such that the new link is smooth.
-#' This means that the exponentiated regression coefficients can be interpreted as relative risks
-#' for individuals whose probability is between cutover[1] and cutover[2] both with and without
+#' This function returns a link function which which equals link function link1 for
+#' values of the fitted probability below cutover. When the fitted probability is greater
+#' than cutover, a linearly scaled user-specified linkis used.
+#' The linear scaling is such that the new link is smooth (link and first derivative both continuous).
+#' The main use of the function is to use an easily interpretable link in binary regression up to
+#' a cutoff (e.g. log), and a more traditional link which asymptotes to 1 above the cutoff (e.g. logit).
+#' The advantage is that the exponentiated regression coefficients can be interpreted as relative risks
+#' for individuals whose probability is up to cutover both with and without
 #' the risk factor. In practice this is usually the great majority of cases.
 #' The function is not normally called directly. Instead, glm or glm2 should be
 #' used (preferably the latter to avoid fitting problems), as per the example below.
-#' @param otherlinkname Character string indicating the link function to be used below cutover[1]
-#'  or above cutover[2]
-#' @param cutover The link function switches smoothly from otherlinkname to log at cutover[1] and back to
-#' otherlinkname at cutover[2]
+#' @param link1 Character string indicating the link function to be used up to the cutover
+#' @param link2 Character string indicating the link function to be used above the cutover
+#' @param cutover The link function switches smoothly from link1 to link2
 #' @return An object of class "link-glm", with link name "blended".
 #' @import glm2
 #' 
@@ -26,34 +27,26 @@
 #' @examples
 #' library(glm2)
 #' example.binary.regression <- glm2(y~x1+x2 , data=loglogit.example,
-#'     family = binomial(link=blended.linkobject(cutover=c(0.2,0.8),otherlinkname="logit")))
+#'     family = binomial(link=blendedLink("log","logit",0.8)))
 #' summary(example.binary.regression)
 #' @export
-blended.linkobject <- function(otherlinkname,cutover){
-  # calculate linear scalers a1, a2, b1 and b2 first
-  a1.plus.b1.logcutover <- make.link(otherlinkname)$linkfun(cutover[1])
-  b1 <- cutover[1] / make.link(otherlinkname)$mu.eta(a1.plus.b1.logcutover)
-  a1 <- a1.plus.b1.logcutover - b1*log(cutover[1])
-  a2.plus.b2.logcutover <- make.link(otherlinkname)$linkfun(cutover[2])
-  b2 <- cutover[2] / make.link(otherlinkname)$mu.eta(a2.plus.b2.logcutover)
-  a2 <- a2.plus.b2.logcutover - b2*log(cutover[2])
+blendedLink <- function(link1,link2,cutover){
+  # calculate linear scalers a and b first
+  L1 <- make.link(link1) ; L2 <- make.link(link2)
+  b <- L1$mu.eta(L1$linkfun(cutover)) / L2$mu.eta(L2$linkfun(cutover))
+  a <- L2$linkfun(cutover) - b*L1$linkfun(cutover)
   # now create and return link components
   linkfun <- function(mu){
-    (mu>=cutover[1])*(mu<=cutover[2])*log(mu) + (mu<cutover[1])*(make.link(otherlinkname)$linkfun(mu)-a1)/b1 +
-      (mu>cutover[2])*(make.link(otherlinkname)$linkfun(mu)-a2)/b2
+    (mu<=cutover)*L1$linkfun(mu) + (mu>cutover)*(L2$linkfun(mu)-a)/b
   }
   linkinv <- function(eta){
-    (eta>=log(cutover[1]))*(eta<=log(cutover[2]))*exp(eta) +
-      (eta<log(cutover[1]))*make.link(otherlinkname)$linkinv(a1+b1*eta) +
-      (eta>log(cutover[2]))*make.link(otherlinkname)$linkinv(a2+b2*eta)
+    (L1$linkinv(eta)<=cutover)*L1$linkinv(eta) + (L1$linkinv(eta)>cutover)*L2$linkinv(a+b*eta)
   }
   mu.eta <- function(eta){
-    (eta>=log(cutover[1]))*(eta<=log(cutover[2]))*exp(eta) +
-      (eta<log(cutover[1]))*make.link(otherlinkname)$mu.eta(a1+b1*eta)*b1 +
-      (eta>log(cutover[2]))*make.link(otherlinkname)$mu.eta(a2+b2*eta)*b2
+    (L1$linkinv(eta)<=cutover)*L1$mu.eta(eta) + (L1$linkinv(eta)>cutover)*L2$mu.eta(a+b*eta)*b
   }
   valideta <- function(eta){
-    rep(T,length(eta))
+    L1$valideta(pmin(eta,L1$linkfun(cutover))) & L2$valideta(pmax(eta,L1$linkfun(cutover)))
   }
   out <- list(  linkfun=linkfun , linkinv=linkinv , mu.eta=mu.eta , valideta=valideta ,
                 name="blended" )
--- R/loglogit.valideta.R
+++
@@ -1,8 +0,0 @@
-#' The valideta item of the loglogit link object
-#' 
-#' For internal use only.
-#' @param eta The linear predictors, a numeric vector of real values.
-#' @return  a logical vector of the same length as eta, with all values equal to TRUE
-loglogit.valideta <- function(eta){
-  rep(T,length(eta))
-}
